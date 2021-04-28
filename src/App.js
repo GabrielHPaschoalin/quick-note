4375491338ee94707d2e2dfee7eb18516873694c
Gabriel Paschoalin <gabrieldias@cpejr.com.br>
2021-04-28 12:08:40 -0300

    Aula 9 - Parte 1

--- src/App.js
+++ src/App.js
@@ -1,38 +1,104 @@
-import { prettyDOM } from '@testing-library/dom';
-import React from 'react';
-import Membro from './Membro ( Aula 8)';
-import Title from './Title - Aula 8';
-
-const Gabriel = {
-  nome: "Gabriel Henrique",
-  idade: 18,
-  curso: "ECA",
-  cor: "azul",
-}
-const Bruna = {
-  nome: "Bruna Eduarda",
-  idade: 19,
-  curso: "contabilidade",
-  cor: "preto",
-}
-const Gustavo = {
-  nome: "Gustavo Henrique",
-  idade: 18,
-  curso: "Cinema",
-  cor: "preto",
-}
+import React from "react";
+import { useState } from "react";
 
 function App() {
 
+    const [nome, setNome] = useState(); //Retorna um vetor com 2 parâmetros
+  const [curso, setCurso] = useState();
+  const [idade, setIdade] = useState();
+  const [Cidade, setCidade] = useState();
+  const [Estado, setEstado] = useState();
+  const [Turno, setTurno] = useState();
+
   return (
     <div>
-      <Title title = "Membros" subtitle = "Esses são os membros">
-      <Membro membro = {Gabriel}/>
-      <Membro membro = {Bruna}/>
-      <Membro membro = {Gustavo}/>
-      </Title>
+
+      <h1>Novo Membro</h1>
+
+      <input //Campo nome
+        type="text"
+        placeholder="Nome"
+        onChange={(event) => setNome(event.target.value)}
+      />
+      <br />
+
+      <input //Engenharia de controle e automação
+        id="ECA"
+        type="radio"
+        name="curso"
+        value="Engenharia de Controle e Automação"
+        onChange={(e) => setCurso(e.target.value)}
+      />
+
+      <label htmlFor="ECA"> Engenharia de Controle e Automação </label>
+
+      <br />
+
+      <input // Campo Engenharia elétrica
+        id="EE"
+        type="radio"
+        name="curso"
+        value="Engenharia Elétrica"
+        onChange={(e) => setCurso(e.target.value)}
+      />
+
+      <label htmlFor="EE">Engenharia Elétrica</label>
+
+      <br />
+
+      <input //Idade
+        type="text"
+        placeholder="Idade"
+        onChange={(event) => setIdade(event.target.value)}
+      />
+
+      <br />
+
+      <input //Estado
+        type="text"
+        placeholder="Estado"
+        onChange={(e) => setEstado(e.target.value)}
+
+      />
+
+      <input
+        id="Diurno"
+        type="radio"
+        name="turno"
+        value="Diurno"
+        onChange={(e) => setTurno(e.target.value)}
+      />
+      <label htmlFor="Diurno"> Diurno </label>
+
+      <input
+        id="Noturno"
+        type="radio"
+        name="turno"
+        value="Noturno"
+        onChange={(e) => setTurno(e.target.value)}
+      />
+      <label htmlFor="Noturno"> Noturno </label>
+
+      <br />
+
+      <input //Cidade
+        type="text"
+        placeholder="Cidade"
+        onChange={(e) => setCidade(e.target.value)}
+
+      />
+
+      <br />
+
+      <p>Nome do membro: {nome}</p>
+      <p>Curso do membro:  {curso}</p>
+      <p>Turno: {Turno}</p>
+      <p>Idade: {idade}</p>
+      <p>Cidade - Estado : {Cidade} - {Estado}</p>
+
+
     </div>
-    );
+  );
 }
 
 export default App;
