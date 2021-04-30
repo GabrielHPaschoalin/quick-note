ca3ee1f807325baaf91794927bdc9b66ca9ab52f
Gabriel Paschoalin <gabrieldias@cpejr.com.br>
2021-04-30 13:17:02 -0300

    Aula 15 - navegação

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
-function Home (){
+function Home() {
 
-    return <h1>Home</h1>;
+    const history = useHistory();
 
+    return (
+        <div>
+            <h1>Home</h1>
+            <button onClick={()=>{history.push("login");}}>Logout</button>
+        </div>
+    );
 
 }
 
