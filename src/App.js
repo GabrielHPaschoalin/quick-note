66be5b3249b8f951a54bd5ad81db9fcfc74d0b76
Gabriel Paschoalin <gabrieldias@cpejr.com.br>
2021-05-01 10:36:01 -0300

    HTML CSS

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from "react";
 import Routes from "./routes";
+import "./global.css"; //Ocupar a tela toda
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App (){
   return <Routes/>;
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,13 +1,25 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import "./Login.css"
+import { Form, Button } from "react-bootstrap"
 
-function Login (){
-
+function Login() {
     return (
-    <div>
-        <h1>Login</h1>
-        <Link to = "home">Home</Link>
-    </div>
+        <div className="base">
+            <div className="container">
+                <div>
+                    <img src="/images/sabado.png" alt="lol" />
+                    <Form className="inputs">
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Control type="email" placeholder="Email" />
+                        </Form.Group>
+                        <Form.Group controlId="Senha">
+                            <Form.Control type="password" placeholder="Senha" />
+                        </Form.Group>
+                        <Button variant="primary">Login</Button>{' '}
+                    </Form>
+                </div>
+            </div>
+        </div>
     );
 }
 
